5c97b0ef873a9aa82203e665b7bf9942fca1d436
Marcel Haupt <subcortexx@gmail.com>
2016-03-08 16:15:21 +0100

    Network class added

--- networking.cpp
+++ networking.cpp
@@ -7,3 +7,106 @@
 //
 
 #include "networking.hpp"
+
+
+Networking::Networking()
+{
+    
+}
+
+
+Networking::~Networking()
+{
+    
+}
+
+#ifdef __APPLE__
+bool Networking::initSocket(std::string ip_adress, unsigned int port)
+{
+    //empty data
+    bzero(&clientAddr, sizeof(clientAddr));
+    
+    //input client data
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_port = port;
+    clientAddr.sin_addr.s_addr = inet_addr(ip_adress.c_str());
+    
+    //create udp socket
+    netSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    if (netSocket < 0) {
+        std::cerr << "ERROR: Could not open Socket " << ip_adress << ":" << port << std::endl;
+        return false;
+    }
+    
+    return true;
+}
+#endif
+
+
+#ifdef __APPLE__
+void Networking::closeSocket()
+{
+}
+#endif
+
+
+#ifdef _WIN32
+bool Networking::initSocket(std::string ip_adress, unsigned int port)
+{
+    int wsaError = WSAStartup( MAKEWORD(2,2), &wsaDat );
+    
+    if(wsaError)
+    {
+        std::cerr << "ERROR: wsa Startup error!" << std::endl;
+        return false;
+    }
+    
+    netSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        
+    if (netSocket == INVALID_SOCKET)
+    {
+        std::cerr << "ERROR: socket function failed with error: " << WSAGetLastError() << std::endl;;
+        return false;
+    }
+        
+    //reset client adress struct
+    ZeroMemory(&clientAddr, sizeof(clientAddr));
+    
+    //settings
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_port = htons(port);
+    clientAddr.sin_addr.s_addr = inet_addr(ip_adress);
+    
+    //create socket
+    netSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    return true;
+}
+#endif
+
+
+
+#ifdef _WIN32
+void Networking::closeSocket()
+{
+    closesocket(netSocket);
+    WSACleanup();
+}
+#endif
+
+
+void Networking::sendData(void* data, unsigned int size)
+{
+    ssize_t n= sendto(netSocket, data, size, 0, (struct sockaddr *) &clientAddr, sizeof (clientAddr));
+    
+    if (n < 0) {
+        std::cerr << "ERROR: sending bytes" << std::endl;
+    }
+}
+
+
+long Networking::receiveData(void* data, unsigned int size)
+{
+    return recvfrom(netSocket, data, size, 0, NULL, NULL);
+}
--- networking.hpp
+++ networking.hpp
@@ -20,8 +20,46 @@
  Pro Client eine Instanz
  */
 
-class Networking {
+//Include the essential header-files
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
+
+#ifdef __APPLE__
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#endif
+
+#include <iostream>
+#include <string>
+
+
+class Networking
+{
+private:
+#ifdef _WIN32
+    SOCKET netSocket;
+    WSADATA wsaData;
+#endif
     
+#ifdef __APPLE__
+    int netSocket;
+#endif
+    
+    sockaddr_in clientAddr;
+    
+public:
+    Networking();
+    ~Networking();
+    
+    bool initSocket(std::string ip_adress, unsigned int port);
+    void sendData(void* data, unsigned int size);
+    long receiveData(void* data, unsigned int size);
+    void closeSocket();
 };
+
 
 #endif /* networking_hpp */
