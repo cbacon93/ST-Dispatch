574a3fcb15d8f8cb03934862b3b20c157e843667
Marcel Haupt <subcortexx@gmail.com>
2016-03-09 13:31:18 +0100

    GUI Finished

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include "networking.hpp"
-#include "bitmap_image.hpp"
 
 
 int initGL();
@@ -20,8 +19,9 @@ void draw();
 bool sim_reset = false;
 bool sim_running = false;
 GLFWwindow* window;
-bitmap_image image_running, image_stopped;
 
+const int WIDTH = 300;
+const int HEIGHT = 150;
 
 
 int main () {
@@ -29,13 +29,15 @@ int main () {
     if (initGL()) return -1;
     std::cout << "Window initialized, starting program" << std::endl;
     
-    //loading images
-    //image_running = bitmap_image("/Users/marcel/Documents/Coding/ST-Dispatch/ST-Dispatch/Running.bmp");
-    //image_stopped = bitmap_image("/Users/marcel/Documents/Coding/ST-Dispatch/ST-Dispatch/Stopped.bmp");
     
     //main loop
-    do {
+    do
+    {
+        
+        
         draw();
+        
+        
     } // Check if the ESC key was pressed or the window was closed
     while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0);
@@ -49,10 +51,49 @@ int main () {
 
 
 void draw() {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //clear background screen to green
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
     
-    glDrawPixels(image_running.width(), image_running.height(), GL_RGB, GL_UNSIGNED_BYTE, image_running.data());
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    
+    //stop
+    if (sim_running)
+        glColor3f(1.0, 0.0, 0.0);
+    else
+        glColor3f(0.5, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.00, 0.00, 0.0);
+    glVertex3f(0.50, 0.00, 0.0);
+    glVertex3f(0.50, 0.50, 0.0);
+    glVertex3f(0.00, 0.50, 0.0);
+    glEnd();
+    
+    //start
+    if (!sim_running)
+        glColor3f(0.0, 1.0, 0.0);
+    else
+        glColor3f(0.0, 0.5, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.00, 0.50, 0.0);
+    glVertex3f(0.50, 0.50, 0.0);
+    glVertex3f(0.50, 1.00, 0.0);
+    glVertex3f(0.00, 1.00, 0.0);
+    glEnd();
+    
+    //reset
+    if (!sim_reset)
+        glColor3f(1.0, 1.0, 0.0);
+    else
+        glColor3f(0.5, 0.5, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.50, 0.00, 0.0);
+    glVertex3f(1.00, 0.00, 0.0);
+    glVertex3f(1.00, 1.00, 0.0);
+    glVertex3f(0.50, 1.00, 0.0);
+    glEnd();
+
+    glFlush();
     
     // Swap buffers
     glfwSwapBuffers(window);
@@ -70,8 +111,9 @@ int initGL() {
         std::cerr << "Failed to initialize GLFW\n";
         return 1;
     }
+
     
-    window = glfwCreateWindow(300, 150, "ST-Dispatch", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "ST-Dispatch", NULL, NULL);
     if (window == NULL) {
         std::cerr << "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n";
         glfwTerminate();
@@ -89,6 +131,24 @@ int initGL() {
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-        sim_running = !sim_running;
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        double px, py;
+        glfwGetCursorPos(window, &px, &py);
+        
+        //std::cout << "Cursor Position: " << px << " / " << py << std::endl;
+        
+        if (px < WIDTH/2 && py < HEIGHT/2 && !sim_running) {
+            std::cout << "starting simulation" << std::endl;
+            sim_running = true;
+        }
+        if (px < WIDTH/2 && py >= HEIGHT/2 && sim_running) {
+            std::cout << "stopping simulation" << std::endl;
+            sim_running = false;
+        }
+        if (px >= WIDTH/2 && !sim_reset) {
+            std::cout << "resetting simulation" << std::endl;
+            sim_reset = true;
+        }
+    }
+    
 }
